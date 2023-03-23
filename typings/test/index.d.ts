c42445e3413815daea855af34a9132c6007ca63c
Jihan Lee <alan@sync.day>
2023-03-23 14:54:14 +0900

    Add typings

---
+++ typings/test/index.d.ts
@@ -0,0 +1,6 @@
+declare const expect: Chai.ExpectStatic;
+// declare const sinon: sinon.SinonStatic;
+
+// declare const stub: TypeORMFaker.StubStatic;
+// declare const stubOne: TypeORMFaker.StubOneStatic;
+// declare const typeormFaker: TypeORMFaker.TypeORMFakerStatic;
