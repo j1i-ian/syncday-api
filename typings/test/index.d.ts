7feb62e0b4c57d55b235b081d4984353d8ee95ba
Jihan Lee <alan@sync.day>
2023-03-23 16:03:28 +0900

    Recover typeorm-faker settings

--- typings/test/index.d.ts
+++ typings/test/index.d.ts
@@ -1,6 +1,6 @@
 declare const expect: Chai.ExpectStatic;
 declare const sinon: sinon.SinonStatic;
 
-// declare const stub: TypeORMFaker.StubStatic;
-// declare const stubOne: TypeORMFaker.StubOneStatic;
-// declare const typeormFaker: TypeORMFaker.TypeORMFakerStatic;
+declare const stub: TypeORMFaker.StubStatic;
+declare const stubOne: TypeORMFaker.StubOneStatic;
+declare const typeormFaker: TypeORMFaker.TypeORMFakerStatic;
