aa4a3af78e58d6b1fa9478ab765e6398824bee05
Jihan Lee <alan@sync.day>
2023-03-23 15:19:33 +0900

    test: Enable sinon global type

--- typings/test/index.d.ts
+++ typings/test/index.d.ts
@@ -1,5 +1,5 @@
 declare const expect: Chai.ExpectStatic;
-// declare const sinon: sinon.SinonStatic;
+declare const sinon: sinon.SinonStatic;
 
 // declare const stub: TypeORMFaker.StubStatic;
 // declare const stubOne: TypeORMFaker.StubOneStatic;
