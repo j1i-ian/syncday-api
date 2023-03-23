ffa452e68757b8a5703a0eede6fee0f137815578
Jihan Lee <alan@sync.day>
2023-03-23 14:51:54 +0900

    Add mocha bootload

---
+++ scripts/mocha-bootload.ts
@@ -0,0 +1,10 @@
+import * as chai from 'chai';
+
+import * as chaiAsPromised from 'chai-as-promised';
+
+// import * as sinon from 'sinon';
+
+chai.use(chaiAsPromised);
+
+// (globalThis as any).sinon = sinon;
+// (globalThis as any).stubQueryBuilder = typeormFaker.stubQueryBuilder;
