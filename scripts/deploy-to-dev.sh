d7bf2bd25a6eb84ee7a7e48a6fc12c98ca2c2242
alan <alan@sync.day>
2023-04-04 13:40:03 +0000

    Resolve "CI/CD pipelines for development/staging environment"

---
+++ scripts/deploy-to-dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PEM_PATH=./syncday.pem
+
+ssh -o StrictHostKeyChecking=no -i $PEM_PATH ubuntu@$1 "cd dev-syncday && rm -rf dist"
+scp -o StrictHostKeyChecking=no -i $PEM_PATH package.json package-lock.json .env.development  ubuntu@$1:/home/ubuntu/dev-syncday
+scp -o StrictHostKeyChecking=no -ri $PEM_PATH $(pwd)/dist ubuntu@$1:/home/ubuntu/dev-syncday/dist
+
+ssh -o StrictHostKeyChecking=no -i $PEM_PATH ubuntu@$1 "cd dev-syncday && npm i --production && pm2 restart --update-env dev-syncday"
