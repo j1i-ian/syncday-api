a7344acf7a8babb0198a7280935bacfeb59c44cc
Jihan Lee <alan@sync.day>
2023-04-06 19:13:21 +0900

    Remove debug logging

--- src/main/services/users/user-setting/user-setting.service.ts
+++ src/main/services/users/user-setting/user-setting.service.ts
@@ -5,13 +5,11 @@ import { UpdateUserSettingDto } from '@dto/users/user-settings/dto/update-user-s
 @Injectable()
 export class UserSettingService {
     async fetchUserSetting(userId: number): Promise<UserSetting> {
-        console.log(userId);
         return Promise.resolve({} as UserSetting);
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async update(userId: number, updateUserSettingDto: UpdateUserSettingDto): Promise<boolean> {
-        console.log(updateUserSettingDto);
         return Promise.resolve(true);
     }
 }
