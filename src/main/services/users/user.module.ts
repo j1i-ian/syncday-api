d575c460c84403ce2e3ba1a55e259f142f3ba1e7
Jihan Lee <alan@sync.day>
2023-04-06 17:15:05 +0900

    Complement integration module import

--- src/main/services/users/user.module.ts
+++ src/main/services/users/user.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@entity/users/user.entity';
 import { TokenModule } from '../../auth/token/token.module';
@@ -7,13 +7,15 @@ import { VerificationModule } from '../../auth/verification/verification.module'
 import { UserSettingModule } from './user-setting/user-setting.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { IntegrationsModule } from '../integrations/integrations.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([User, UserSetting]),
         UserSettingModule,
         TokenModule,
-        VerificationModule
+        VerificationModule,
+        forwardRef(() => IntegrationsModule)
     ],
     controllers: [UserController],
     providers: [UserService],
