4e6f1f5c939d01974c1b8a82ed74f8d8c8db1bcd
collin <hubigo97@gmail.com>
2023-04-07 07:08:45 +0000

    Resolve "implement get user setting api"

---
+++ src/main/dto/users/fetch-user-info-response.dto.ts
@@ -0,0 +1,29 @@
+import { Expose } from 'class-transformer';
+import { Language } from '../../enums/language.enum';
+import { IntegrationsInfo } from '../../services/users/interfaces/integrations-info.interface';
+
+export class FetchUserInfoResponseDto {
+    @Expose()
+    name: string;
+
+    @Expose()
+    link: string;
+
+    @Expose()
+    greetings: string;
+
+    @Expose()
+    language: Language;
+
+    @Expose()
+    dateTimeFormat: Intl.DateTimeFormatOptions;
+
+    @Expose()
+    dateTimeOrderFormat: string;
+
+    @Expose()
+    timeZone: string;
+
+    @Expose()
+    integration: IntegrationsInfo;
+}
