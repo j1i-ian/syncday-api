01968073a4c5d9c663d516e5a99214575d26e103
DU <du@sync.day>
2023-04-11 09:14:50 +0000

    Resolve "BE) 구글 캘린더 연결 설정"

--- src/main/services/integrations/integrations.module.ts
+++ src/main/services/integrations/integrations.module.ts
@@ -5,9 +5,11 @@ import { GoogleIntegration } from '../../../@core/core/entities/integrations/goo
 import { GoogleIntegrationsService } from './google-integrations.service';
 import { IntegrationsService } from './integrations.service';
 import { CalendarsModule } from './calendars/calendars.module';
+import { IntegrationsController } from './integrations.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([GoogleIntegration]), ConfigModule, CalendarsModule],
+    controllers: [IntegrationsController],
     providers: [IntegrationsService, GoogleIntegrationsService],
     exports: [IntegrationsService, GoogleIntegrationsService]
 })
