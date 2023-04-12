91cfb34bbb90590cb3a0e9cd127ec121e4af816a
alan <alan@sync.day>
2023-04-12 05:07:11 +0000

    Resolve "workspace 변경 작업"

--- src/main/services/users/user-setting/user-setting.controller.ts
+++ src/main/services/users/user-setting/user-setting.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Header, Param, Patch, Put } from '@nestjs/common';
+import { Body, Controller, Get, Header, Patch, Put } from '@nestjs/common';
 import { UserSetting } from '@core/entities/users/user-setting.entity';
 import { UpdateUserSettingRequestDto } from '@dto/users/update-user-setting-request.dto';
 import { AuthUser } from '../../../decorators/auth-user.decorator';
 import { AppJwtPayload } from '../../../auth/strategy/jwt/app-jwt-payload.interface';
+import { PatchUserSettingRequestDto } from '../../../dto/users/user-settings/update-user-workspace-request.dto';
 import { UserSettingService } from './user-setting.service';
 
 @Controller()
@@ -10,38 +11,31 @@ export class UserSettingController {
     constructor(private readonly userSettingService: UserSettingService) {}
 
     @Get()
-    async fetchUserSetting(userId: number): Promise<UserSetting> {
+    async fetchUserSetting(@AuthUser() { id: userId }: AppJwtPayload): Promise<UserSetting> {
         return this.userSettingService.fetchUserSetting(userId);
     }
 
-    @Put(':id(\\d+)')
-    @Header('Content-type', 'application/json')
-    async updateUserSetting(
-        @Param('id') id: string,
-        @Body() updateUserSettingDto: UpdateUserSettingRequestDto
-    ): Promise<boolean> {
-        return await this.userSettingService.updateUserSetting(+id, updateUserSettingDto);
-    }
-
-    @Get(':workspace')
-    @Header('Content-type', 'application/json')
-    fetchWorkspaceStatus(@Param('workspace') workspace: string): Promise<boolean> {
-        const workspaceStatus = this.userSettingService.fetchUserWorkspaceStatus(workspace);
-
-        return workspaceStatus;
-    }
-
-    @Patch(':workspace')
+    @Patch(':userId(\\d+)')
     @Header('Content-type', 'application/json')
     async updateWorkspace(
-        @Param('workspace') workspace: string,
+        @Body() putUserSettingRequestDto: PatchUserSettingRequestDto,
         @AuthUser() { id }: AppJwtPayload
     ): Promise<boolean> {
+        const { workspace } = putUserSettingRequestDto;
         const workspaceStatus = await this.userSettingService.createUserWorkspaceStatus(
             id,
             workspace
         );
 
         return workspaceStatus;
+    }
+
+    @Put(':userId(\\d+)')
+    @Header('Content-type', 'application/json')
+    async updateUserSetting(
+        @AuthUser() { id: userId }: AppJwtPayload,
+        @Body() updateUserSettingDto: UpdateUserSettingRequestDto
+    ): Promise<boolean> {
+        return await this.userSettingService.updateUserSetting(+userId, updateUserSettingDto);
     }
 }
