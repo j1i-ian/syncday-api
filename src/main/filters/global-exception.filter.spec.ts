2afe5f6709c129970bfc65465cf7bfe6b803e451
alan <alan@sync.day>
2023-05-05 11:56:48 +0900

    Remove Hangul check

--- src/main/filters/global-exception.filter.spec.ts
+++ src/main/filters/global-exception.filter.spec.ts
@@ -6,34 +6,4 @@ describe('GlobalExceptionFilter', () => {
     it('Global filter initialization test', () => {
         expect(globalFilterException).ok;
     });
-
-    describe('Internal server error check', () => {
-        [
-            {
-                description:
-                    'In the case of a Korean error message, the error message must not be changed.',
-                message: '영어로만 되어있는 에러 메시지일 경우 시스템의 것일 가능성이 큽니다.',
-                expected: false
-            },
-            {
-                description:
-                    'If the error message is only in English, it must be properly converted.',
-                message:
-                    'Do not pass internal error message. It can be malformed by crackers easily.',
-                expected: true
-            },
-            {
-                description:
-                    'In the case of an error message in English only, it must be properly converted.',
-                message: '요청변수가 적합하지 않습니다.(query - 주소를 상세히 입력해 주십시오.)',
-                expected: false
-            }
-        ].forEach(({ description, message, expected }) => {
-            it(description, () => {
-                const result = globalFilterException.isInternalErrorEnglishMessage(message);
-
-                expect(result).equals(expected);
-            });
-        });
-    });
 });
--- src/main/filters/global-exception.filter.ts
+++ src/main/filters/global-exception.filter.ts
@@ -28,25 +28,15 @@ export class GlobalExceptionFilter implements ExceptionFilter {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
 
-        let message = exception.message;
-
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const _responseMessages = (exception as any).response?.message;
-
-        if (_responseMessages && Array.isArray(_responseMessages)) {
-            message = _responseMessages.join('');
-        }
+        let message = (exception as any).response?.message;
 
         const status = (exception as HttpException).getStatus?.() || 500;
 
-        const isInternalErrorMessage = this.isInternalErrorEnglishMessage(message);
-
-        if (isInternalErrorMessage) {
-            if (status / 100 === 5) {
-                message = 'Server error happend.';
-            } else if (exception instanceof UnauthorizedException) {
-                message = 'Unauthoized Information.';
-            }
+        if (status / 100 === 5) {
+            message = 'Server error happend.';
+        } else if (exception instanceof UnauthorizedException) {
+            message = 'Unauthoized Information.';
         }
 
         this.logger.error({
@@ -57,25 +47,7 @@ export class GlobalExceptionFilter implements ExceptionFilter {
 
         response.status(status).json({
             statusCode: status,
-            msg: message
+            message
         });
-    }
-
-    /**
-     *
-     * 전체 에러메시지의 한글 비율이 30% 미만일 경우 영어 에러 메시지로 간주한다.
-     *
-     * @param message 한글인지 영어인지 모를 에러메시지
-     * @returns
-     */
-    isInternalErrorEnglishMessage(message: string | undefined): boolean {
-        const requireHangulPercentage = 30;
-
-        const isEnglishErrorMessage = message
-            ? (message.replace(/[ㄱ-힣\s]*/g, '').length / message.length) * 100 >
-              requireHangulPercentage
-            : true;
-
-        return isEnglishErrorMessage;
     }
 }
