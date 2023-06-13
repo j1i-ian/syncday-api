44229c32ba2618a31ca3dfdb97a8e634b0be3841
alan <alan@sync.day>
2023-06-13 17:44:44 +0900

    Add node env for local:dev

--- src/configs/node-env.enum.ts
+++ src/configs/node-env.enum.ts
@@ -5,6 +5,10 @@ export enum NodeEnv {
 
     DEVELOP = 'development',
 
+    LOCAL_PRODUCTION = 'local:production',
+
+    LOCAL_DEVELOP = 'local:development',
+
     LOCAL = 'local',
 
     TEST = 'test'
