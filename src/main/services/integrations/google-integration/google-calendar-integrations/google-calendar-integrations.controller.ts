3c8763cc678cb0c715a690fcdba816f4faea79fc
alan <alan@sync.day>
2023-07-12 16:07:53 +0900

    Remove test code

--- src/main/services/integrations/google-integration/google-calendar-integrations/google-calendar-integrations.controller.ts
+++ src/main/services/integrations/google-integration/google-calendar-integrations/google-calendar-integrations.controller.ts
@@ -1,7 +1,6 @@
 import { BadRequestException, Controller, Headers, Inject, Logger, Param, Post } from '@nestjs/common';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { GoogleCalendarIntegrationsService } from '@services/integrations/google-integration/google-calendar-integrations/google-calendar-integrations.service';
-import { GoogleCalendarEventWatchStopService } from '@services/integrations/google-integration/facades/google-calendar-event-watch-stop.service';
 import { IntegrationUtilsService } from '@services/util/integration-utils/integration-utils.service';
 import { Public } from '@app/auth/strategy/jwt/public.decorator';
 
@@ -54,28 +53,9 @@ export class GoogleCalendarIntegrationsController {
         } else {
             // TODO: unsubscribe orphan calendar
             this.logger.warn('Orphan Calendar is detected. You should implement the code for unsubscription');
-
-            await this.tmp(xGoogChannelId, xGoogResourceId);
         }
 
 
         return true;
-    }
-
-    async tmp(xGoogChannelId: string,
-        xGoogResourceId: string): Promise<void> {
-
-        const refreshToken = '1//0eJ9NfRsykyVpCgYIARAAGA4SNwF-L9IrvGv_mBVigN22yH7v8PExeK6APWYhcVH1I1WZk5VYeRLaBQbLy4G7deFEtOs-MfhxwIk';
-        const newOAuthClient = this.integrationUtilsService.getGoogleOAuthClient(refreshToken);
-
-        const googleCalendarEventWatchStopService = new GoogleCalendarEventWatchStopService();
-
-        console.log('unsubscribe orphan...');
-        await googleCalendarEventWatchStopService.stopWatch(
-            newOAuthClient,
-            xGoogChannelId,
-            xGoogResourceId
-        );
-
     }
 }
