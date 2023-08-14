0e64395dce99f45e98124efc42a51b0c83d60942
alan <alan@sync.day>
2023-08-14 09:49:46 +0900

    Update dev deploy script

--- scripts/deploy-to-dev.sh
+++ scripts/deploy-to-dev.sh
@@ -6,4 +6,4 @@ ssh -o StrictHostKeyChecking=no -i $PEM_PATH ubuntu@$1 "cd dev-syncday && rm -rf
 scp -o StrictHostKeyChecking=no -i $PEM_PATH package.json package-lock.json .env.dev ecosystem.dev.json ubuntu@$1:/home/ubuntu/dev-syncday
 scp -o StrictHostKeyChecking=no -ri $PEM_PATH $(pwd)/dist ubuntu@$1:/home/ubuntu/dev-syncday/dist
 
-ssh -o StrictHostKeyChecking=no -i $PEM_PATH ubuntu@$1 "cd dev-syncday && npm install --ignore-scripts && pm2 restart --update-env dev-syncday-api"
+ssh -o StrictHostKeyChecking=no -i $PEM_PATH ubuntu@$1 "cd dev-syncday && npm install --ignore-scripts && pm2 restart ecosystem.dev.json --env dev"
