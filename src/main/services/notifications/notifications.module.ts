a53eff6e8a9c58642e499f82222ade4a2380b845
alan <alan@sync.day>
2023-11-19 23:48:08 +0000

    Draft: Resolve "New Kakao Alim Talk: Manual Share"
    Squashed: '562-new-kakao-alim-talk-manual-share-2' into 'develop'
    
    See related MR syncday/syncday-api!390

--- src/main/services/notifications/notifications.module.ts
+++ src/main/services/notifications/notifications.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { NotificationsService } from '@services/notifications/notifications.service';
+import { EventsModule } from '@services/events/events.module';
+import { SyncdayAwsSdkClientModule } from '@services/util/syncday-aws-sdk-client/syncday-aws-sdk-client.module';
 import { NotificationsController } from './notifications.controller';
 
 @Module({
-    imports: [ConfigModule],
+    imports: [
+        ConfigModule,
+        EventsModule,
+        SyncdayAwsSdkClientModule
+    ],
     controllers: [NotificationsController],
     providers: [NotificationsService],
     exports: [NotificationsService]
