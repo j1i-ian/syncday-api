7d9b989fa2245aa07019168597c94b4f128b10eb
Alan Lee <alanlee@sync.day>
2024-06-04 10:07:11 +0900

    Remove unused code in health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
-import { InjectDataSource } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
 import { Public } from './main/auth/strategy/jwt/public.decorator';
 
 @Controller()
 export class AppController {
-
-    constructor(
-        @InjectDataSource() private readonly datasource: DataSource
-    ) {}
 
     @Public()
     @Get()
